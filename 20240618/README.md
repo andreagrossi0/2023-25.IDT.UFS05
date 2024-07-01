9dec0031886c62d4200ff59bf663af63cd39fc76
andrea <156222632+andreagrossi0@users.noreply.github.com>
2024-07-01 09:57:19 +0000

    giusto

--- 20240618/README.md
+++ 20240618/README.md
@@ -5,12 +5,12 @@ Following the official how to
 https://learn.microsoft.com/en-us/azure/app-service/provision-resource-terraform
 
 
-export TF_VAR_AZURE_RESOURCE_GROUP=learn-...
-export TF_VAR_AZURE_APP_SERVICE_REPO_URL='....'
+export TF_VAR_AZURE_RESOURCE_GROUP=learn-d6b9583c-4aaf-4c91-81fc-08ec88a281a2
+export TF_VAR_AZURE_APP_SERVICE_REPO_URL='https://github.com/andreagrossi0/2023-25.IDT.UFS05'
 
 terraform init
 
-terraform import azurerm_resource_group.rg '...'
+terraform import azurerm_resource_group.rg /subscriptions/d66e7c68-0149-4c50-ad72-c2d5c26dc60e/resourcegroups/learn-d6b9583c-4aaf-4c91-81fc-08ec88a281a2
 
 az webapp log tail --name '...' --resource-group $TF_VAR_AZURE_RESOURCE_GROUP
 
--- 20240618/main.tf
+++ 20240618/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_resource_group" "rg" {
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
+  location            = var.AZURE_REGION
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "S1"
@@ -49,7 +49,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "nodered" {
   name                = "nodered-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
+  location            = var.AZURE_REGION
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
   https_only          = true
@@ -71,7 +71,7 @@ resource "azurerm_linux_web_app" "nodered" {
 
 resource "azurerm_linux_web_app" "python" {
   name                = "python-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
+  location            = var.AZURE_REGION
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
   https_only          = true
@@ -92,4 +92,17 @@ resource "azurerm_app_service_source_control" "python_scm" {
   branch   = "master"
   use_manual_integration = true
   use_mercurial      = false
+}
+resource "azurerm_mysql_flexible_server" "example" {
+  name                   = "its-rizzoli-idt-mysql-${random_integer.ri.result}"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = var.AZURE_REGION
+  administrator_login    = "psqladmin"
+  // WARN DONT DO THIS, USE SecOps services like Doppler and Azure Key Vault
+  administrator_password = "H@Sh1CoR3!"
+  sku_name = "GP_Standard_D2ds_v4"
+}
+
+output "mysql_fqdn" {
+  value = azurerm_mysql_flexible_server.example.fqdn
 }
